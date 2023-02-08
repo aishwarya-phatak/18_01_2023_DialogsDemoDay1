3c518f8106ee316b136a8a371fd40ae60b36b78c
aishwarya <ashp17star@gmail.com>
2023-02-08 13:29:29 +0530

    AlertDialog

--- DialogsDemoVersion1/app/src/main/java/com/example/dialogsdemoversion1/MainActivity.java
+++ DialogsDemoVersion1/app/src/main/java/com/example/dialogsdemoversion1/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.dialogsdemoversion1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnDatePickerDialog,btnTimePickerDialog,btnAlertDialog;
@@ -35,6 +38,84 @@ public class MainActivity extends AppCompatActivity {
     private void initListeners(){
         btnDatePickerDialog.setOnClickListener(new BtnDatePickerClickListener());
         btnTimePickerDialog.setOnClickListener(new BtnTimePickerClickListener());
+        btnAlertDialog.setOnClickListener(new BtnAlertClickListener());
+    }
+
+    class BtnAlertClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Android Batch");
+            builder.setMessage("Have you completed the tasks?");
+            builder.setIcon(R.drawable.ic_launcher_background);
+
+            /*builder.setPositiveButton("Yes",new BtnPositiveClickListener());
+            builder.setNeutralButton("NA",new BtnNeutralClickListener());
+            builder.setNegativeButton("No",new BtnNegativeClickListener());
+             */
+            DialogInterface.OnClickListener listener = new AlertDialogButtonsClickListener();
+            builder.setPositiveButton("Yes",listener);
+            builder.setNeutralButton("NA",listener);
+            builder.setNegativeButton("No",listener);
+
+            builder.setOnCancelListener(new BtnOnCancelClickListener());
+            builder.setOnDismissListener(new BtnOnDismissClickListener());
+
+            AlertDialog loginDialog = builder.create();
+            loginDialog.setCancelable(true);
+
+            loginDialog.show();
+        }
+    }
+
+    class AlertDialogButtonsClickListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if(which == Dialog.BUTTON_POSITIVE){
+                mt("on positive btn clicked -- $ of which -->" + which);
+            }
+            if(which == Dialog.BUTTON_NEUTRAL){
+                mt("on positive btn clicked -- $ of which -->" + which);
+            }
+            if(which == Dialog.BUTTON_NEGATIVE){
+                mt("on positive btn clicked -- $ of which -->" + which);
+            }
+        }
+    }
+
+    class BtnOnCancelClickListener implements DialogInterface.OnCancelListener{
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            mt("onCancel called");
+        }
+    }
+
+    class BtnOnDismissClickListener implements DialogInterface.OnDismissListener{
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            mt("onDismiss called");
+        }
+    }
+
+    class BtnPositiveClickListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            mt("Positive Btn Clicked -- $which" + which);
+        }
+    }
+
+    class BtnNeutralClickListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            mt("Neutral Btn Clicked -- $which" + which);
+        }
+    }
+
+    class BtnNegativeClickListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            mt("Negative Btn Clicked -- $which" + which);
+        }
     }
 
 
@@ -85,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
+    public void mt(String text){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
 }
